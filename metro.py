41f6faf6463fe90f98829d7290e027fdb3c1fed7
Дмитрий Карих <the.dr.hax@gmail.com>
2016-02-15 10:00:28 +0300

    Изменение алгоритма и проверки подключения
    * снова добавлен поиск скрытой формы после авторизации (лишним не будет, с ним работает)
    
    * проверка соединения с интернетом теперь производится путем поиска перенаправления со страницы vmet.ro (issue #7)

--- metro.py
+++ metro.py
@@ -8,7 +8,8 @@ from HTMLParser import HTMLParser
 
 # Массив данных для POST запросов
 post_data = dict()
-
+# Массив header'ов
+headers = {}
 
 # Парсер полей input из HTML
 class FormInputParser(HTMLParser):
@@ -29,17 +30,7 @@ def try_connect(address):
 
 
 # Функция авторизации
-def connect():
-    # Пока не разобрался с сессиями, будет хранилище header'ов
-    headers = {}
-
-    # Получаем перенаправление
-    page_vmetro = requests.get('http://vmet.ro')
-    headers.update({'referer': page_vmetro.url})
-
-    # Вытаскиваем назначение редиректа
-    url_auth = re.search('https?:[^\"]*', page_vmetro.text).group(0)
-
+def connect(url_auth):
     # Запрашиваем страницу с кнопкой авторизации
     page_auth = requests.get(url_auth, headers=headers,
                              cookies=page_vmetro.cookies,
@@ -50,12 +41,20 @@ def connect():
     parser = FormInputParser()
     parser.feed(re.search("<body>.*?</body>",
                           page_auth.content, re.DOTALL).group(0))
-
+    
     # Отправляем полученную форму
     page_postauth = requests.post(url_auth, data=post_data,
                                   cookies=page_auth.cookies,
                                   headers=headers, verify=False)
+    
+    # Парсим поля второй формы (необходимость неизвестна, но так работает)
+    parser.feed(re.search("<body>.*?</body>",
+                          page_auth.content, re.DOTALL).group(0))
 
+    # Отправляем вторую полученную форму
+    page_postauth2 = requests.post(url_auth, data=post_data,
+                                   cookies=page_auth.cookies,
+                                   headers=headers, verify=False)
 
 if __name__ == '__main__':
     print(datetime.now())
@@ -63,8 +62,15 @@ if __name__ == '__main__':
     # "Пингуем" роутер
     if try_connect("http://1.1.1.1/login.html"):
         for counter in range(3):
-            # HTTPS не позволяет провайдеру влезть с редиректом
-            if try_connect("https://wtfismyip.com/text"):
+            try:
+                # Получаем перенаправление
+                page_vmetro = requests.get('http://vmet.ro', verify=False)
+                headers.update({'referer': page_vmetro.url})
+
+                # Вытаскиваем назначение редиректа
+                url_auth = re.search('https?:[^\"]*', page_vmetro.text).group(0)
+            
+            except requests.exceptions.ConnectionError:
                 if counter == 0:
                     print("Already connected")
                 else:
@@ -72,7 +78,8 @@ if __name__ == '__main__':
                 break
 
             try:
-                connect()
+                print("Connecting...")
+                connect(url_auth)
             except requests.exceptions.ConnectionError:
                 print("Connection failed")
 
