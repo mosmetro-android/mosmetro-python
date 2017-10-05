7437bf22d55333af7b3a0a4ccb6983cf7b40cddf
Дмитрий Карих <the.dr.hax@gmail.com>
2017-10-05 21:12:18 +0300

    MosMetroV2: Port algorithm from mosmetro-android

--- metro.py
+++ metro.py
@@ -2,9 +2,11 @@
 # -*- coding: UTF-8 -*-
 
 import sys
+from urlparse import urlparse, urljoin, parse_qs
 from datetime import datetime
 
 import requests
+from pyquery import PyQuery
 
 class Provider(object):
     def __init__(self, session):
@@ -35,14 +37,53 @@ class Provider(object):
         for provider in Provider.__subclasses__():
             if (provider.match(r)):
                 print("Detected provider: " + provider.__name__)
-                return provider(session);
+                return provider(session, r);
 
         print("Wrong network")
         return False
 
 class MosMetroV2(Provider):
-    def __init__(self, session):
-        self.session = session
+    def __init__(self, session, response):
+        super(MosMetroV2, self).__init__(session)
+        self.response = response
+
+    def connect(self):
+        print("Parsing initial redirect")
+        redirect = urlparse(self.response.headers.get("Location"))
+
+        if "segment" in redirect.query:
+            segment = parse_qs(redirect.query)["segment"][0]
+        else:
+            segment = "metro"
+
+        # Check if device is not registered
+        if "identification" in redirect.path:
+            print("Registration is required. Please go to " + redirect.geturl())
+            return False
+
+        print("Following initial redirect")
+        r = session.get(redirect.geturl(), allow_redirects=False)
+
+        if r.status_code in (301, 302) and "auto_auth" in r.headers.get("Location"):
+            print("You probably have been temporary banned.")
+            return False
+
+        print("Following JavaScript redirect")
+        r = session.get(urljoin(redirect.geturl(), "/auth?segment=" + segment))
+
+        # Parsing CSRF token
+        csrf = PyQuery(r.content)("meta[name=csrf-token]").attr("content")
+        session.headers["X-CSRF-Token"] = csrf
+
+        # Setting additional Cookies (probably required)
+        requests.Session().cookies.set("afVideoPassed", "0")
+
+        print("Sending auth request")
+        r = session.get(urljoin(redirect.geturl(),
+                                "/auth/init?mode=0&segment=" + segment))
+
+        print("Checking internet connection")
+        return Provider.generate_204(session) is True
 
     @staticmethod
     def match(response):
@@ -61,3 +102,10 @@ if __name__ == '__main__':
 
         if p is True or p is False:
             sys.exit(p)
+
+        if p.connect():
+            print("Connected successfully! :3")
+            sys.exit(0)
+        else:
+            print("Connection failed :(")
+            sys.exit(1)
